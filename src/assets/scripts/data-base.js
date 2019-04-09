a4d6452e4e5939d0e636972adfc302ab14368751
Roman Lickevich <r.lickevich@gmail.com>
2019-04-09 23:21:11 +0200

    добавил миксины

--- src/assets/scripts/data-base.js
+++ src/assets/scripts/data-base.js
@@ -13,111 +13,46 @@ function refreshDate() {
     return db;
 }
 
-const rd = refreshDate();
-
-// console.log(rd);
-
-
-const templ = [
-    {
-        date: "1 Ноября",
-        items: [
-            {
-                id: 564564867361360,
-                type: "Приход",
-                item: [
-                    {
-                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
-                        name: "Молочный 200",
-                        price: 180,
-                        quantity: 4,
-                        removed: 1
-                    },
-                    {
-                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
-                        name: "Груша 200",
-                        price: 186,
-                        quantity: 5,
-                        removed: 1
-                    }
-                ]
-            },
-            {
-                id: 564564867361361,
-                type: "Расход",
-                item: [
-                    {
-                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
-                        name: "Молочный Груша",
-                        price: 111,
-                        quantity: 12,
-                        removed: 1
-                    },
-                    {
-                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
-                        name: "Молочный",
-                        price: 222,
-                        quantity: 14,
-                        removed: 0
-                    }
-                ]
-            }
-        ]
-    }
-]
-
-// const wList = document.querySelector('#whouse__list');
-
-// function test() {
-//     let obj = {};
-//         obj.list = rd;
-
-//     const template = require('../templates/whouse/whouse-item.hbs');
-//     const result = template(obj);
-
-//     wList.innerHTML = result;
-//     console.log(result);
-// }
-// test();
-
 function sortRemoved() {
+    const rd = refreshDate();
     let arr = [];
 
     for (let i = 0; i < rd.length; i++) {
-        if (rd[i].removed != 0) {
+        if (rd[i].removed != 1) {
             arr.push(rd[i]);
         }
     }
     return arr;
 }
 
-const sortRem = sortRemoved();
-
 function results() {
-    const result = {}
+    const sortRem = sortRemoved();
+    const result = {};
 
     sortRem.forEach(item => {
 
         if (!result[item.date]) {
             result[item.date] = {
                 date: item.date,
-                items: {},
+                items: {}
             }
         }
-            if (!result[item.date].items[item.id]) {
-                result[item.date].items[item.id] = {
-                    type: item.type,
-                    id: item.id,
-                    item: []
-                }
-            }
 
-            result[item.date].items[item.id].item.push({
-                image: item.image,
-                name: item.name,
-                price: item.price,
-                quantity: item.quantity
-            })
+        if (!result[item.date].items[item.id]) {
+            result[item.date].items[item.id] = {
+                type: item.type,
+                id: item.id,
+                item: []
+            }
+        }
+
+        result[item.date].items[item.id].item.push({
+            image: item.image,
+            name: item.name,
+            price: item.price,
+            quantity: item.quantity,
+            total: (Math.round((item.price * item.quantity) * 100) / 100)
+        })
     })
 
     return Object.keys(result).map(key => {
@@ -125,5 +60,18 @@ function results() {
         return result[key]
     });
 }
-results();
-console.log(results());
+
+function test() {
+    const wList = document.querySelector('#whouse__list');
+    const resultDB = results();
+    let obj = {};
+        obj.list = resultDB;
+
+    console.log(obj);
+
+    const template = require('../templates/whouse/whouse-item.hbs');
+    const result = template(obj);
+
+    wList.innerHTML = result;
+}
+test();
