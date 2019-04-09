b1e5065f2fc9795d8d5dda9f87de07082f377ddf
Roman Lickevich <r.lickevich@gmail.com>
2019-04-09 18:46:32 +0200

    массив отсортирован

--- src/assets/scripts/data-base.js
+++ src/assets/scripts/data-base.js
@@ -15,34 +15,115 @@ function refreshDate() {
 
 const rd = refreshDate();
 
-console.log(rd);
+// console.log(rd);
 
-// const templ = {
-//     date: '1 Ноября',
-//     id: {
-//         564564867361360: {
-//             type: 'Приход',
-//             image: ['ссылка 1', 'ссылка 2'],
-//             name: ['название 1', 'название 2'],
-//             price: ['цена 1', 'цена 2'],
-//             quantity: ['количество 1', 'количество 2'],
-//             removed: [0, 1]
-//         },
-//         564564867361361: {
-//             type: 'Расход',
-//             image: ['ссылка 3', 'ссылка 4'],
-//             name: ['название 3', 'название 4'],
-//             price: ['цена 3', 'цена 4'],
-//             quantity: ['количество 3', 'количество 4'],
-//             removed: [1, 1]
-//         }
-//     }
-// }
 
-function dataBase() {
-    let array = [];
-    for (let i = 0; i < rd.length; i++) {
-
+const templ = [
+    {
+        date: "1 Ноября",
+        items: [
+            {
+                id: 564564867361360,
+                type: "Приход",
+                item: [
+                    {
+                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
+                        name: "Молочный 200",
+                        price: 180,
+                        quantity: 4,
+                        removed: 1
+                    },
+                    {
+                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
+                        name: "Груша 200",
+                        price: 186,
+                        quantity: 5,
+                        removed: 1
+                    }
+                ]
+            },
+            {
+                id: 564564867361361,
+                type: "Расход",
+                item: [
+                    {
+                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
+                        name: "Молочный Груша",
+                        price: 111,
+                        quantity: 12,
+                        removed: 1
+                    },
+                    {
+                        image: "https://images.ua.prom.st/50389447_w640_h640_ncheskaya_s_grushej_200_g..jpg",
+                        name: "Молочный",
+                        price: 222,
+                        quantity: 14,
+                        removed: 0
+                    }
+                ]
+            }
+        ]
     }
+]
+
+// const wList = document.querySelector('#whouse__list');
+
+// function test() {
+//     let obj = {};
+//         obj.list = rd;
+
+//     const template = require('../templates/whouse/whouse-item.hbs');
+//     const result = template(obj);
+
+//     wList.innerHTML = result;
+//     console.log(result);
+// }
+// test();
+
+function sortRemoved() {
+    let arr = [];
+
+    for (let i = 0; i < rd.length; i++) {
+        if (rd[i].removed != 0) {
+            arr.push(rd[i]);
+        }
+    }
+    return arr;
 }
-dataBase();
+
+const sortRem = sortRemoved();
+
+function results() {
+    const result = {}
+
+    sortRem.forEach(item => {
+
+        if (!result[item.date]) {
+            result[item.date] = {
+                date: item.date,
+                items: {},
+            }
+        }
+            if (!result[item.date].items[item.id]) {
+                result[item.date].items[item.id] = {
+                    type: item.type,
+                    id: item.id,
+                    item: []
+                }
+            }
+
+            result[item.date].items[item.id].item.push({
+                image: item.image,
+                name: item.name,
+                price: item.price,
+                quantity: item.quantity
+            })
+    })
+
+    return Object.keys(result).map(key => {
+        result[key].items = Object.keys(result[key].items).map(key2 => result[key].items[key2])
+        return result[key]
+    });
+}
+results();
+console.log(results());
